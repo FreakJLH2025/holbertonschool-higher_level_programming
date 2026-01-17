15a158a57b72a58d8b9978271ab41f60e3167a67
FreakJLH2025 <joshua.luja@gmail.com>
2026-01-17 22:03:24 +0000

    7-islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    """Check if a character is lowercase."""
-    if len(c) > 0 and ord('a') <= ord(c) <= ord('z'):
+    if ord('a') <= ord(c) <= ord('z'):
         return True
     return False
