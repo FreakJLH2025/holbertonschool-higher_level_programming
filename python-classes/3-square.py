ddf17213845641229d773f1a5bbe2b540c5282cc
FreakJLH2025 <joshua.luja@gmail.com>
2026-01-27 16:35:36 +0000

    3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+The class represents a geometric square with a private size attribute.
+It validates that size is a non-negative integer and can compute its area.
+"""
+
+
+class Square:
+    """
+    This class defines a square by its size.
+    The size attribute is private to ensure control
+    over its type and value.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the square (default is 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        Compute the area of the square.
+
+        Returns:
+            int: The current square area.
+        """
+        return self.__size * self.__size
