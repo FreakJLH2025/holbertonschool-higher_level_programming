e6766bde74a27b6b44ac131de2d082968a0a9598
FreakJLH2025 <joshua.luja@gmail.com>
2026-02-03 18:14:12 +0000

    11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module that defines the Square class.
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square with private size,
+        validated as a positive integer.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        Returns the area of the Square.
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        Returns the string representation of the Square.
+        Format: [Square] <width>/<height>
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
