edd51f82022c925c26691d27940a23d0d7d99178
FreakJLH2025 <joshua.luja@gmail.com>
2026-03-05 17:44:22 +0000

    6-states.sql

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,14 @@
+-- Script that creates the database hbtn_0d_usa and the table states
+-- states description:
+--   id INT unique, auto generated, not null, primary key
+--   name VARCHAR(256) not null
+-- If the database hbtn_0d_usa already exists, the script should not fail
+-- If the table states already exists, the script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
